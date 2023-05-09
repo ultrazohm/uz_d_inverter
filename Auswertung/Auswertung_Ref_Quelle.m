a28b634f4040520370d27eea37da6b11bb7ecd74
Dennis Hufnagel <dennis.hufnagel@th-nuernberg.de>
2023-05-09 15:38:59 +0200

    Fixes distance between mounting holes

--- Auswertung/Auswertung_Ref_Quelle.m
+++ Auswertung/Auswertung_Ref_Quelle.m
@@ -1,15 +1,22 @@
 A = readmatrix("scope_210.csv");
 
-plot(A(3:2002,1), A(3:2002,2), A(3:2002,1), A(3:2002,5));
-
+figure
+subplot(2,1,1)
 x = A(3:2002,1);
 y = A(3:2002,2);
-yyaxis left
-xlabel('time in s')
-plot (x,y);
-ylabel('Spannung in U')
+title('Voltage during turn-on')
+p1=plot (x,y);
+p1.Color = "#e41a1c"
+ylabel('Voltage in V')
+xlabel('Time in s')
 y2=A(3:2002,5)+0.01;
+hold on
+subplot(2,1,2)
+p2=plot(x,y2);
+p2.Color = "#377eb8"
+title({'';'Current during turn-on'})
+ylabel('Current in A')
+xlabel('Time in s')
 
-yyaxis right
-plot(x,y2);
-ylabel('Storm in A')
+
+ 
